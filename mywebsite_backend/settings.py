7f8ff753bf5f82d890e873db26d331d9031d0275
shrazo <saddam.hossain.raju.024@gmail.com>
2024-06-26 03:33:57 +0800

    update settings.py

--- mywebsite_backend/settings.py
+++ mywebsite_backend/settings.py
@@ -29,6 +29,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # 3rd party
+    'rest_framework',
+    "corsheaders",
+    'django_quill',
 
     # Local
     "mainapp",
